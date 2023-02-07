bfa7192fe396d87fe49483353198cf5379f8286c
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-07 12:39:05 +0100

    3

---
+++ HomeWork23/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Задать номер четверти, показать диапазоны для возможных координат*/
+Console.WriteLine("Введите номер четверти: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+    Console.WriteLine("диапазоны для возможных координат: x > 0, y > 0");
+else if (a == 2)
+    Console.WriteLine("диапазоны для возможных координат: x < 0, y > 0");
+else if (a == 3)
+    Console.WriteLine("диапазоны для возможных координат: x < 0 , y < 0");
+else if (a == 4)
+    Console.WriteLine("диапазоны для возможных координат: x > 0, y < 0");
